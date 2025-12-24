86033379d7ca7518cc071bf83f793bc012e3d48e
25eqsg3f08-stack <25eqsg3f08@g.elctp.k12.edu.mo>
2025-12-24 20:34:47 +0800

    更新 offlinecache.js

--- js/offlinecache.js
+++ js/offlinecache.js
@@ -5,7 +5,8 @@ const CORE_ASSETS = [
     '/',
     '/index.html',
     '/js/main.js',
-    '/js/photo1.jpg', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+    '/js/photo1.jpg',
+    "/js/offlinecache.js", 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
 // 安装：缓存核心静态资源
